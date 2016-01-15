4d58a271c73e49dcb4ae6e61f233bf455965da8e
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-01-15 10:30:00 +0900

    add jquery value on setting.js

--- browseraction/setting.js
+++ browseraction/setting.js
@@ -1,1 +1,42 @@
-alert("hello!");
+$(function(){
+    
+alert(jQuery);//ok
+$github_checkbox=$("#githubon");
+$local_checkbox=$("#localon");
+
+
+$ltime=$("#localtime");
+$gtime=$("#githubtime");
+$url=$("#url");
+$repo=$("#repo");
+$pass=$("#pass");
+$username=$("#username");
+
+$local_checkbox.click(background);
+$github_checkbox.click(background);
+function background(e){
+    //onにすると設定ができるようになる。
+}
+$(".save").click(check);
+function check(e){
+    if(check_to_github()){
+        
+    //あったら保存
+    }else{
+        
+    //なければダイアログを出す。
+        
+    }
+    
+}
+function check_to_github(){
+    //username,password,ripoを読みだし,fetchで確認。
+    if(1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+    
+    
+});
