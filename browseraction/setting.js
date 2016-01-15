f02c92947f3ca0394e63b579e90d6b757cc3de94
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-01-16 01:59:00 +0900

    githubAPIからデータを取ってきてリポジトリの名前を取得して判断するところまでadd

--- browseraction/setting.js
+++ browseraction/setting.js
@@ -1,81 +1,85 @@
-$(function(){
+$(function () {
     
-//alert(jQuery);//ok
-$github_checkbox=$("#githubon");
-$local_checkbox=$("#localon");
+    //alert(jQuery);//ok
+    $github_checkbox = $("#githubon");
+    $local_checkbox = $("#localon");
 
 
-$ltime=$("#localtime");
-$gtime=$("#githubtime");
-$url=$("#url");
-$url.click(function (e){
-    //クリックされると<input>が増える。
-    //tureはeventもコピーするか
-    $(this).clone(true).insertAfter(this);
-})
-$repo=$("#repo");
-$pass=$("#pass");
-$username=$("#username");
+    $ltime = $("#localtime");
+    $gtime = $("#githubtime");
+    $url = $("#url");
+    $url.click(function (e) {
+        //クリックされると<input>が増える。
+        //tureはeventもコピーするか
+        $(this).clone(true).insertAfter(this);
+    })
+    $repo = $("#repo");
+    $pass = $("#pass");
+    $username = $("#username");
 
-$local_checkbox.click(background);
-$github_checkbox.click(background);
-function background(e){
-    //onにすると設定ができるようになる。
-}
-$(".save").click(checkmain);
-
-function checkmain(e){
-    console.log("fe");
-    sendgithub()
-}
-
-function sendgithub(){
-    console.log("fefe");
-    var user=$("#username").text();
-    
-   var xhr = new XMLHttpRequest();
-
-    console.log("fefef");
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-      
-      var res=JSON.parse(this.responseText);
-      dataparse(res)
-
-  }
-});
-
-xhr.open("GET", "https://api.github.com/users/"+user+"repos");
-xhr.send(null);
-     
-}
-
-
-function dataparse(res){
-    //xhrで帰ってきたオブジェクトを整形して確認する。
-    console.log(res);
-    console.log(res.name);
-
-    
-    if(1){
-        return 1;
-    }else{
-        return 0;
+    $local_checkbox.click(background);
+    $github_checkbox.click(background);
+    function background(e) {
+        //onにすると設定ができるようになる。
     }
-}
-    
-    
-});
+    $(".save").click(checkmain);
 
-
-function last(bool){
-    if(0){
-        
-    //あったら保存
-    }else{
-        
-    //なければダイアログを出す。
-    //エラーが出てる部分の上に赤い文字でエラーを出したり、inputを赤くする。
+    function checkmain(e) {
+        sendgithub()
     }
+
+    function sendgithub() {
+        console.log("fefe");
+        var user = $("#username").val();//.text();
+        //inputにはval()を使う
+        var xhr = new XMLHttpRequest();
+
+        console.log($("#username"));
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+
+                var res = JSON.parse(this.responseText);
+                last(dataparse(res))
+
+            }
+        });
+
+        xhr.open("GET", "https://api.github.com/users/" + user + "/repos");
+        xhr.send(null);
+
+    }
+
+
+    function dataparse(res) {
+        let flag = false;
     
-}
+        //xhrで帰ってきたオブジェクトを整形して確認する。
+        console.log(res);
+        //配列にonjが入っている。
+        //console.log(res.name);
+        for (let data of res) {
+            alert(data.name + "   " + $("#repo").val())
+            if (data.name === $("#repo").val()) {
+                alert("ありますねぇ！");
+                flag = true;
+            }
+            return flag;
+        }
+    }
+
+
+        function last(bool) {
+            if (0) {
+        
+                //あったら保存
+            } else {
+        
+                //なければダイアログを出す。
+                //エラーが出てる部分の上に赤い文字でエラーを出したり、inputを赤くする。
+            }
+
+        }
+
+    });
+
+
